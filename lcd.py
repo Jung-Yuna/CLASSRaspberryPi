ed83ede3b76f5bbceaee9f360eb619d16c88d70c
Jung-Yuna <youna0518@naver.com>
2021-05-31 08:58:37 +0100

    SW+LCD

--- lcd.py
+++ lcd.py
@@ -13,17 +13,18 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(LED, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(Switch, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 while True:
-    # GPIO.input(Switch)와 GPIO.HIGH를 AND 연산으로 변경 가능
     if GPIO.input(Switch) == GPIO.HIGH:
         print("Button was pushed!")
         GPIO.output(LED, GPIO.HIGH)
         mylcd.lcd_display_string("LED ON",1)
         time.sleep(3)
-        
+        mylcd.lcd_clear
+        sleep(0.2)
     else:
         print("Button was not pushed!")
         GPIO.output(LED, GPIO.LOW)
         mylcd.lcd_display_string("LED OFF",1)
         time.sleep(1)
-        
+        mylcd.lcd_clear
+        sleep(0.2)
 
